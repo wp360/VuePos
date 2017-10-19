7cba227bc596c0f6360f2822a5b44933cf67ca13
wp360 <2897406527@qq.com>
2017-10-19 15:20:03 +0800

    first commit

---
+++ src/components/common/leftNav.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="left-nav">
+        <ul>
+            <li>
+                <i class="icon iconfont icon-wodezichan"></i>
+                <div>收银</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-jinrudianpu"></i>
+                <div>店铺</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-hanbao"></i>
+                <div>商品</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-31huiyuanqia"></i>
+                <div>会员</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-tongji"></i>
+                <div>统计</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-31shezhi"></i>
+                <div>设置</div>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style>
+    .left-nav{
+        color: white;
+        font-size: 12px;
+        width: 5%;
+        height: 100%;
+        background-color: #1D8CE0;
+        float: left;
+    }
+    .iconfont{
+        font-size: 24px;
+    }
+    .left-nav ul{
+        padding: 0;
+        margin: 0;
+    }
+    .left-nav li{
+        list-style: none;
+        text-align: center;
+        border-bottom: 1px solid #20A0FF;
+        padding: 10px;
+    }
+</style>
+
---
+++ src/components/page/Pos.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="pos">
+      <el-row>
+          <el-col :span='7' class="pos-order" id="order-list">
+              订单栏区域
+          </el-col>
+          <el-col>
+              产品栏区域
+          </el-col>
+      </el-row>
+  </div>
+</template>
+<script>
+    export default{
+        name: 'pos',
+        mounted: function(){
+            var orderHeight = document.body.clientHeight;
+            //console.log(orderHeight);
+            document.getElementById('order-list').style.height = orderHeight + 'px';
+        }
+    }
+</script>
+<style scoped>
+    .pos-order{
+        background-color: #F9FAFC;
+        border: 1px solid #C0CCDA;
+        height: 100%;
+    }
+</style>
+
